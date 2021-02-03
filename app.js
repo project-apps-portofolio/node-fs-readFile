b988b3e9e2d93165e24159a047e91dd65129ac7a
ilyas <ilyas@mewahniagajaya.com>
2021-02-03 14:46:13 +0700

    delete live reload on app

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const bodyParser = require('body-parser');
-const livereload = require("livereload");
-const connectLivereload = require("connect-livereload");
 
 var app = express();
 
